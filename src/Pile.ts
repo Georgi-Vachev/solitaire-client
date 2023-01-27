73c34884f63d3f4257808cccddc9c76d10eb3589
Emil Dyulgerov <emil.dyulgerov@dyulgerov-len>
2023-01-27 20:50:53 +0200

    Init Pile class

---
+++ src/Pile.ts
@@ -0,0 +1,31 @@
+import * as PIXI from 'pixi.js'
+
+export class StockPile {
+    public deck: PIXI.DisplayObject[];
+    private app: PIXI.Application;
+    public container: PIXI.Container;
+    private x: number;
+    private y: number;
+
+    constructor(deck: PIXI.DisplayObject[], app: PIXI.Application, x: number, y: number){
+        this.deck = deck;
+        this.app = app;
+        this.x = x;
+        this.y = y;
+        this.container = new PIXI.Container();
+        this.container.interactive = true;
+    }
+
+    drawPile(){
+        const pileBorder = new PIXI.Graphics();
+                pileBorder.lineStyle(2, 0xFFCC00, 1);
+                pileBorder.drawRoundedRect(0, 0, 95, 140, 5);
+                pileBorder.endFill();
+                this.container.position.set(this.x, this.y);
+                this.container.pivot.set(47.5, 70)
+                this.container.addChild(pileBorder);
+                this.app.stage.addChild(this.container);
+    }
+    
+    
+}
